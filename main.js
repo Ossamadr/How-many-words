fbfa3a08ea786d55831d4deced282da1948de4b5
Ossama Drarouiche <98663338+Ossamadr@users.noreply.github.com>
2024-02-05 14:58:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-let btnLetter = document.getElementById("btnLetter");
-let btnWord = document.getElementById("btnWord");
-let word = document.getElementById("word");
-let letter = document.getElementById("letter");
-let inp = document.getElementById("inp");
+ let btnLetter = document.getElementById("btnLetter");
+        let btnWord = document.getElementById("btnWord");
+        let word = document.getElementById("word");
+        let letter = document.getElementById("letter");
+        let inp = document.getElementById("inp");
 
-btnLetter.onclick = () => {
-    let valueInp = inp.value
-    let sum = valueInp.split("").filter((el) => {
-        return el !== " "
-    }).length
-    letter.innerHTML = sum
-}
-btnWord.onclick = () => {
-    let valueInp = inp.value
-    let sum = valueInp.split(" ").length
-    word.innerHTML = sum
-}
+        btnLetter.onclick = () => {
+            let valueInp = inp.value;
+            let lettersOnly = valueInp.replace(/[^a-zA-Z]/g, ''); // Remove non-alphabetic characters
+            let sum = lettersOnly.length;
+            letter.innerHTML = sum;
+        }
+
+        btnWord.onclick = () => {
+            let valueInp = inp.value;
+            let sum = valueInp.split(" ").length;
+            word.innerHTML = sum;
+        }
